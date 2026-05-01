174b896fe18678c6eb2cf0fbe5ff55ea9e4814e9
User <User@DESKTOP-4VMO11C>
2026-05-01 21:38:15 +0530

    Add project files.

---
+++ eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Core;
+
+public static class DependencyInjection
+{
+
+    /// <summary>
+    /// Extension method to add core services to the dependency injection container.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        // TODO: Add services to the IoC container, such as database contexts, repositories, etc.
+        return services;
+    }
+}
---
+++ eCommerce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Infrastructure;
+
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Extension method to add infrastructure services to the dependency injection container.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        // TODO: Add services to the IoC container, such as database contexts, repositories, etc.
+        return services;
+    }
+}
